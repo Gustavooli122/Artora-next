deb8a52fd28a4ab37ae0e16265478ffb1f9fcbab
Gustavo Oliveira <gustavooli122@gmail.com>
2026-07-01 18:38:16 -0300

    atualizado

--- src/app/data/product.js
+++ src/app/data/product.js
@@ -230,19 +230,19 @@ A **Wacom One CTL-472** é uma mesa digitalizadora desenvolvida para proporciona
   {
     id: slugify('Kit 6 Esfuminho Esfumador Para Desenho Realista'),
     name: 'Kit 6 Esfuminho Esfumador Para Desenho Realista',
-    description: `# Wacom One CTL-472
+    description: `
 
-A **Wacom One CTL-472** é uma mesa digitalizadora desenvolvida para proporcionar precisão, conforto e praticidade em desenhos, ilustrações, edições de imagem e anotações digitais.
+# Sombra e Luz Profissional Alta Densidade Ponta Dupla Para Grafite Carvão Pastel Seco Esboço Ilustração
 
 ## Principais Características
 
 * SOMBRAS MAIS SUAVES SEM DEPENDER DO DEDO com kit de 6 esfuminhos para espalhar grafite, carvão e pastel seco com mais controle em desenhos realistas, esboços e ilustrações.
-🎨 MAIS PRECISÃO EM LUZ E SOMBRA com pontas duplas de diferentes espessuras que ajudam a trabalhar áreas pequenas, médias e maiores sem perder o acabamento do desenho.
-✏️ KIT COM 6 TAMANHOS PARA DIFERENTES TÉCNICAS incluindo medidas de 11 cm, 12 cm, 12,5 cm, 14 cm, 14,5 cm e 15 cm, todos com 1 cm de largura conforme referência do produto.
-🖌️ MATERIAL DE PAPEL COMPRIMIDO DE ALTA DENSIDADE ideal para artistas, estudantes e iniciantes que desejam esfumar, suavizar traços e criar transições visuais com mais praticidade.
-🚫 EVITE BORRÕES EXCESSIVOS E MARCAS INDESEJADAS usando o esfuminho como apoio técnico para misturar sombras com mais limpeza do que o uso direto dos dedos no papel.
+* MAIS PRECISÃO EM LUZ E SOMBRA com pontas duplas de diferentes espessuras que ajudam a trabalhar áreas pequenas, médias e maiores sem perder o acabamento do desenho.
+* KIT COM 6 TAMANHOS PARA DIFERENTES TÉCNICAS incluindo medidas de 11 cm, 12 cm, 12,5 cm, 14 cm, 14,5 cm e 15 cm, todos com 1 cm de largura conforme referência do produto.
+* MATERIAL DE PAPEL COMPRIMIDO DE ALTA DENSIDADE ideal para artistas, estudantes e iniciantes que desejam esfumar, suavizar traços e criar transições visuais com mais praticidade.
+* EVITE BORRÕES EXCESSIVOS E MARCAS INDESEJADAS usando o esfuminho como apoio técnico para misturar sombras com mais limpeza do que o uso direto dos dedos no papel.
 `,
-    price: 285.32,
+    price: 26.01,
     image: 'https://m.media-amazon.com/images/I/51Mo2+rLHGL._AC_SL1254_.jpg',
     category: 'Esfuminhos',
     rating: 4.8,
